ea184055b83e6fea72f48b44cb4200e5389b1a48
Evilbas <dimonloboto@gmail.com>
2021-07-21 09:07:28 -0400

    timed aspect

--- src/main/java/com/evilbas/metrics/MetricsConfig.java
+++ src/main/java/com/evilbas/metrics/MetricsConfig.java
@@ -1,12 +1,21 @@
 package com.evilbas.metrics;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 @PropertySource("classpath:metrics.properties")
 public class MetricsConfig {
     public static String test() {
         return "Success";
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
     }
 }
